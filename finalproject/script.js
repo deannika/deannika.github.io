f6280dd4ae99810f5197441c654d379df397927d
Dean Nika <deannika81@gmail.com>
2025-12-02 22:27:33 -0500

    Updated script.js with filtered makes

--- finalproject/script.js
+++ finalproject/script.js
@@ -1,4 +1,4 @@
-// Load modern car makes (2020–2024 only)
+// Load modern car makes (2020–2024)
 async function loadMakes() {
     var dropdown = document.getElementById("makeDropdown");
     var message = document.getElementById("makeMessage");
@@ -9,25 +9,42 @@ async function loadMakes() {
     if (message) message.innerHTML = "";
 
     try {
-        // Choose any modern year (change 2020 to 2021/2022/2023/2024 if you want)
-        var url = "https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?year=2020&format=json";
+        // Years we are combining
+        var years = [2020, 2021, 2022, 2023, 2024];
+        var allMakes = [];        
 
-        var response = await fetch(url);
-        var data = await response.json();
-        var results = data.Results;
+        // Fetch each year one at a time
+        for (var i = 0; i < years.length; i++) {
+            var url = "https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?year=" 
+                      + years[i] + "&format=json";
 
-        // Clear dropdown and add default option
+            var response = await fetch(url);
+            var data = await response.json();
+
+            // Add all results to the list
+            for (var j = 0; j < data.Results.length; j++) {
+                allMakes.push(data.Results[j].Make_Name);
+            }
+        }
+
+        // Remove duplicates by turning into a Set, then back to array
+        var uniqueMakes = Array.from(new Set(allMakes));
+
+        // Sort alphabetically
+        uniqueMakes.sort();
+
+        // Clear dropdown and add default
         dropdown.innerHTML = "<option value=''>-- Select a modern make --</option>";
 
-        // Add each modern make
-        for (var i = 0; i < results.length; i++) {
-            var makeName = results[i].Make_Name;
-            dropdown.innerHTML += "<option value='" + makeName + "'>" + makeName + "</option>";
+        // Add all unique makes to dropdown
+        for (var i = 0; i < uniqueMakes.length; i++) {
+            dropdown.innerHTML += "<option value='" + uniqueMakes[i] + "'>" 
+                + uniqueMakes[i] + "</option>";
         }
 
     } catch (error) {
         dropdown.innerHTML = "<option>Error loading makes</option>";
-        if (message) message.innerHTML = "Could not load modern makes.";
+        if (message) message.innerHTML = "Could not load modern car brands.";
     }
 }
 // the site would keep crashing if I did all the cars so I had to slim the choices down
